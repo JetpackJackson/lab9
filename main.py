e203233ef2074508901d9830c4a89b206d292f2b
katarinaneal <166740315+katarinaneal@users.noreply.github.com>
2024-04-11 12:41:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ def encode(password):
         newchar = str(int(char) + 3)
         encoded_password += str(int(newchar) % 10)
     return encoded_password
+    
+#Katarina Neal
+    def decoder(password):
+    result = ''
+    for digit in password:
+        new_digit = str((int(digit) - 3) % 10)
+        result += new_digit
+    return result
+    
